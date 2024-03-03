79efa74a7302ee66a40072c38176c9d8f6d16d02
tharinxd <21027@students.mrgs.school.nz>
2024-03-04 10:45:21 +1300

    tkinter starter

--- tkinter.py
+++ tkinter.py
@@ -1,5 +1,13 @@
 from tkinter import *
 
 class QuizStarter:
-    def_init_(self, parent):
+    def__init__(self, parent):
     background_color="OldLace"
+
+
+
+
+    if__name__== "__Main__":#
+    root = Tk()
+    root.title("NZ Road Rules Quiz")
+    root.mainloop()
